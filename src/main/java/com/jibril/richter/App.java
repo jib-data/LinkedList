54a8f8f99d31178182d55b748c29a329fca0f618
Jibril Richter <jibrilrichter@gmail.com>
2024-11-27 12:13:30 -0600

    Add binanry search tree data structure

--- src/main/java/com/jibril/richter/App.java
+++ src/main/java/com/jibril/richter/App.java
@@ -8,16 +8,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        DoublyLinkedList dll = new DoublyLinkedList(3);
-        dll.appendNode(4);
-        dll.prependNode(1);
-        dll.appendNode(77);
+//        DoublyLinkedList dll = new DoublyLinkedList(3);
+//        dll.appendNode(4);
+//        dll.prependNode(1);
+//        dll.appendNode(77);
+//
+////        dll.printList();
+////        dll.removeLast();
+//        dll.setNode(3, 100);
+////        dll.printList();
+//        System.out.println( dll.getNode(3).value);
 
-//        dll.printList();
-//        dll.removeLast();
-        dll.setNode(3, 100);
-//        dll.printList();
-        System.out.println( dll.getNode(3).value);
+        BinarySearchTree bst = new BinarySearchTree();
+        System.out.println(bst.getRoot());
 
     }
 }
--- src/main/java/com/jibril/richter/BinarySearchTree.java
+++ src/main/java/com/jibril/richter/BinarySearchTree.java
@@ -1,4 +1,25 @@
 package com.jibril.richter;
 
 public class BinarySearchTree {
+    private Node root;
+
+
+    class Node{
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+    BinarySearchTree(){
+
+    }
+
+
+    public Node getRoot(){
+        return root;
+    }
 }
