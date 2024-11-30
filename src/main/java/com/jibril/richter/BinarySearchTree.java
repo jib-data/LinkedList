3429599e285455e4b8fa892c5187a9e645c95ddb
Jibril Richter <jibrilrichter@gmail.com>
2024-11-30 17:46:56 -0600

    add contains mehtod to BST

--- src/main/java/com/jibril/richter/BinarySearchTree.java
+++ src/main/java/com/jibril/richter/BinarySearchTree.java
@@ -22,4 +22,50 @@ public class BinarySearchTree {
     public Node getRoot(){
         return root;
     }
+
+    public boolean insertNode(int value){
+        Node newNode = new Node(value);
+        if(root == null){
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true){
+            if(temp.value == newNode.value){
+                return false;
+            }
+            if(newNode.value < temp.value){
+                if(temp.left == null){
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            }
+            if(newNode.value > temp.value){
+                if(temp.right == null){
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+
+    }
+    public boolean contains(int value){
+        if(root == null){
+            return false;
+        }
+        Node temp = root;
+
+        while(temp !=null){
+            if(value > temp.value){
+                temp = temp.right;
+            } else if (value < temp.value){
+                temp = temp.left;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
